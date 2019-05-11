6a4b3158e854f775a0534eb9a6ce8edf76e6f26d
ryanboris <ryan.boris@gmail.com>
2019-05-11 03:13:09 -0400

    derps

--- shopping-list/src/index.js
+++ shopping-list/src/index.js
@@ -1,41 +1,39 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
-import "./index.css";
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+import './index.css'
 
 // Bootstrap, MDB, fonts, other style libraries
-import '@fortawesome/fontawesome-free/css/all.min.css';
-import 'bootstrap-css-only/css/bootstrap.min.css';
-import 'mdbreact/dist/css/mdb.css';
+import '@fortawesome/fontawesome-free/css/all.min.css'
+import 'bootstrap-css-only/css/bootstrap.min.css'
+import 'mdbreact/dist/css/mdb.css'
 
 // Router
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router } from 'react-router-dom'
 
 // Redux
-import { createStore, applyMiddleware, compose } from "redux";
-import { Provider } from "react-redux";
-import thunk from "redux-thunk";
-import logger from "redux-logger";
+import { createStore, applyMiddleware, compose } from 'redux'
+import { Provider } from 'react-redux'
+import thunk from 'redux-thunk'
+import logger from 'redux-logger'
 
-
-
-import rootReducer from "./store/reducers/";
+import rootReducer from './store/reducers/'
 
 // Redux dev tools
 const composeEnhancers =
-    typeof window === 'object' &&
-    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?
-        window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
-            // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
-        }) : compose;
+  typeof window === 'object' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+        // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
+      })
+    : compose
 
 const enhancer = composeEnhancers(
-    applyMiddleware(thunk, logger),
-    // other store enhancers if any
-);
-const store = createStore(rootReducer, enhancer);
+  applyMiddleware(thunk, logger)
+  // other store enhancers if any
+)
+const store = createStore(rootReducer, enhancer)
 
-const rootElement = document.getElementById("root");
+const rootElement = document.getElementById('root')
 
 ReactDOM.render(
   <Provider store={store}>
@@ -44,9 +42,4 @@ ReactDOM.render(
     </Router>
   </Provider>,
   rootElement
-);
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+)
